4c4320ec2af53a02d850a07e436b4a9a382f399a
ConfidenceP <mukwindidzacp@gmail.com>
2021-02-06 18:25:59 +0200

    Database Connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const mysql =require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'C108629d',
+    database: 'exchange_rate_db'
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello Confidence!')
-})
+    const sqlInsert = "INSERT INTO exchange_rates('eur', 'usd', 'rate_time') VALUES ('1', '1.2074', '05-02-2021 14:46');"
+    db.query(sqlInsert, (err, result) => {
+        res.send('Hello Confidence!');
+    })
+});
 
 app.listen(3001, () => {
     console.log("running on port 3001")
-})
+});
